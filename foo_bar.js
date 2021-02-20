1bb451d98e6c917ccf2cf50324c284b1f01cbc16
Zach Ferguson <yoshizachz@gmail.com>
2021-02-20 18:12:27 -0500

    added prompts for oogas and boogas

--- foo_bar.js
+++ foo_bar.js
@@ -1,14 +1,19 @@
 function oogabooga(){
 
+//chupa mi culo roberto
+
+    var in1 = prompt("enter number to log 'ooga'", in1);
+    var in2 = prompt("enter number to log 'booga'", in2);
+
     for (let i=0;i<1000;++i){   
-        if(i%15==0){
-            console.log("foobar");
+        if((i%in1==0) && (i%in2==0)){
+            console.log("oogabooga");
         }
-        else if(i%3==0){
-            console.log("foo");
+        else if(i%in1==0){
+            console.log("ooga");
         }
-        else if(i%5==0){
-            console.log("bar");
+        else if(i%in2==0){
+            console.log("booga");
         }
         else{
             console.log(i)
